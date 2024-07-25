2ce5b95563e68f74e2e49c8b52c9d23e1c3752d7
huxiaoou <516984451@qq.com>
2024-07-25 16:14:07 +0800

    Add comparison for pandas interface of HDF5 and sqlite

--- main.py
+++ main.py
@@ -1,4 +1,70 @@
+"""
+conclusion:
+0.  parameter data_columns is a key factor to effect the performance. This parameter means List of columns to create
+    as indexed data columns for on-disk queries, or True to use all columns. By default, only the axes of the object
+    are indexed.
+1.  nrow > 1_000_000, h5 > sql
+2.  nrow <   100_000, h5 < sql
+"""
 if __name__ == "__main__":
-    from test_funs_and_modules import test_the_initialization
+    from husfort.qsqlite import CTable
+    from test_funs_and_modules import create_numpy_array
+    from test_funs_and_modules import test_save_to_h5, test_save_to_sql
+    from test_funs_and_modules import test_view_h5, test_view_sql
+    from test_funs_and_modules import test_select_from_h5, test_select_from_sql
+    from test_funs_and_modules import test_append_to_h5
 
-    test_the_initialization()
+    nrow, ncol = 100_000, 10
+    cnames = [f"C{_:02d}" for _ in range(ncol)]
+    df1 = create_numpy_array(nrow, ncol, cnames=cnames, start_id=0)
+    df2 = create_numpy_array(nrow, ncol, cnames=cnames, start_id=nrow)
+    print(df1)
+    print(df2)
+    h5_file, sql_file = "test.h5", "test.db"
+    dset_name = "my_test_dataset"
+    threshold_date = f"T{int(0.5 * nrow * 2):08d}"
+    data_columns = ["T", "C00", "C01"]
+
+    print("[INF] --- test 1: save ---")
+    test_save_to_h5(df=df1, file_path=h5_file, dset_name=dset_name, data_columns=data_columns)
+    table = CTable(
+        table_struct={
+            "table_name": dset_name,
+            "primary_keys": {"T": "TEXT"},
+            "value_columns": {_: "REAL" for _ in cnames},
+        }
+    )
+    test_save_to_sql(df=df1, file_path=sql_file, table=table, remove_existence=True)
+
+    print("[INF] --- test 2: append ---")
+    test_append_to_h5(df=df2, file_path=h5_file, dset_name=dset_name, data_columns=data_columns)
+    test_save_to_sql(df=df2, file_path=sql_file, table=table, remove_existence=False)
+
+    print("[INF] --- test 3: view ---")
+    df3 = test_view_h5(file_path=h5_file, dset_name=dset_name)
+    df4 = test_view_sql(file_path=sql_file, dset_name=dset_name, values=["T"] + cnames)
+    print(f"[INF] df_h5  shape={df3.shape}")
+    print(f"[INF] df_sql shape={df4.shape}")
+    print(df3.head(5))
+    print(df4.head(5))
+
+    print("[INF] --- test 4: select ---")
+    slc_data1 = test_select_from_h5(
+        file_path=h5_file, dset_name=dset_name,
+        conds=[
+            # "C00 > 0 | C02 > 0",
+            "C00 > 0",
+            "C01 < 0",
+            f"T > {threshold_date}"
+        ])
+    slc_data2 = test_select_from_sql(
+        file_path=sql_file, dset_name=dset_name, values=["T"] + cnames,
+        conds=[
+            ("C00", ">", 0),
+            ("C01", "<", 0),
+            ("T", ">", threshold_date),
+        ])
+    print(f"[INF] selected h5  shape={slc_data1.shape}")
+    print(f"[INF] selected sql shape={slc_data2.shape}")
+    print(slc_data1)
+    print(slc_data2)
--- test_funs_and_modules.py
+++ test_funs_and_modules.py
@@ -1,27 +1,74 @@
-import h5py
-import numpy as np
+import pandas as pd
+import scipy.stats as sps
+from husfort.qutility import qtimer
+from husfort.qsqlite import CManagerLibWriter, CTable, CManagerLibReader
 
 
-def test_the_initialization() -> None:
-    with h5py.File("mytestfile.hdf5", "w") as f:
-        dset = f.create_dataset("mydataset", (100,), dtype='i')
-        sub_grp = f.create_group("subgroup")
-        sub_sub_grp = sub_grp.create_group("subsubgroup")
-        another_dset1 = sub_grp.create_dataset("another_dataset", (100, 8), dtype="f")
-        another_dset2 = sub_sub_grp.create_dataset("another_dataset", (100, 8), dtype="f")
-        print(f.name)
-        print(dset.name)
-        print(dset.shape)
-        print(dset.dtype)
-        print(sub_grp.name)
-        print(sub_sub_grp.name)
-        print(another_dset1.name)
-        print(another_dset2.name)
+def create_numpy_array(nrow: int, ncol: int, cnames: list[str], start_id: int,
+                       random_state: int = None) -> pd.DataFrame:
+    data = sps.norm.rvs(size=(nrow, ncol), loc=0, scale=1, random_state=random_state)
+    df = pd.DataFrame(data=data, columns=cnames)
+    df["T"] = [f"T{_:08d}" for _ in range(start_id, nrow + start_id)]
+    df = df[["T"] + cnames]
+    return df
 
-        print("-" * 12)
-        for name in f:
-            print(name)
 
-        print("-" * 12)
-        print(f"subgroup in f is {'subgroup' in f}")
-        print(f"subsubgroup in f is {'subsubgroup' in f}")
+# --- pandas interface for hdf5
+@qtimer
+def test_save_to_h5(df: pd.DataFrame, file_path: str, dset_name: str, data_columns: list[str] | bool = None):
+    store = pd.HDFStore(file_path, mode="w")
+    store.put(key=dset_name, value=df, format="table", data_columns=data_columns)
+    store.close()
+    return 0
+
+
+@qtimer
+def test_append_to_h5(df: pd.DataFrame, file_path: str, dset_name: str, data_columns: list[str] | bool = None):
+    store = pd.HDFStore(file_path, mode="a")
+    store.append(key=dset_name, value=df, format="table", data_columns=data_columns)
+    store.close()
+    return 0
+
+
+@qtimer
+def test_view_h5(file_path: str, dset_name: str) -> pd.DataFrame:
+    store = pd.HDFStore(file_path, mode="r")
+    df: pd.DataFrame = store.get(key=dset_name)  # type:ignore
+    store.close()
+    return df
+
+
+@qtimer
+def test_select_from_h5(file_path: str, dset_name: str, conds: list | None = None) -> pd.DataFrame:
+    store = pd.HDFStore(file_path, mode="r")
+    df: pd.DataFrame = store.select(key=dset_name, where=conds)  # type:ignore
+    store.close()
+    return df
+
+
+# --- sql methods to be compared ---
+@qtimer
+def test_save_to_sql(df: pd.DataFrame, file_path: str, table: CTable, remove_existence: bool):
+    lib_writer = CManagerLibWriter(db_save_dir=".", db_name=file_path)
+    lib_writer.initialize_table(table=table, remove_existence=remove_existence)
+    lib_writer.update(update_df=df, using_index=False)
+    lib_writer.commit()
+    lib_writer.close()
+    return 0
+
+
+@qtimer
+def test_view_sql(file_path: str, dset_name: str, values: list[str]) -> pd.DataFrame:
+    lib_reader = CManagerLibReader(db_save_dir=".", db_name=file_path)
+    lib_reader.set_default(default_table_name=dset_name)
+    df = lib_reader.read(value_columns=values)
+    return df
+
+
+@qtimer
+def test_select_from_sql(file_path: str, dset_name: str, values: list[str],
+                         conds: list | None = None) -> pd.DataFrame:
+    lib_reader = CManagerLibReader(db_save_dir=".", db_name=file_path)
+    lib_reader.set_default(default_table_name=dset_name)
+    df = lib_reader.read_by_conditions(conditions=conds, value_columns=values)
+    return df
